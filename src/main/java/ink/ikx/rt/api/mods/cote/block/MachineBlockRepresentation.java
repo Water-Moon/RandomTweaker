adbf61dbec30a29757a95cda458c727061b48c99
ikexing <75018198+ikexing-cn@users.noreply.github.com>
2021-08-22 20:11:02 +0800

    difference

---
+++ src/main/java/ink/ikx/rt/api/mods/cote/block/MachineBlockRepresentation.java
@@ -0,0 +1,23 @@
+package ink.ikx.rt.api.mods.cote.block;
+
+import com.teamacronymcoders.base.registrysystem.BlockRegistry;
+import com.teamacronymcoders.contenttweaker.ContentTweaker;
+import com.teamacronymcoders.contenttweaker.modules.vanilla.blocks.BlockRepresentation;
+import crafttweaker.annotations.ModOnly;
+import crafttweaker.annotations.ZenRegister;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenProperty;
+
+@ZenRegister
+@ModOnly("contenttweaker")
+@ZenClass("mods.randomtweaker.cote.Block")
+public class MachineBlockRepresentation extends BlockRepresentation {
+
+    @ZenProperty
+    public String machineName;
+
+    @Override
+    public void register() {
+        ContentTweaker.instance.getRegistry(BlockRegistry.class, "BLOCK").register(new MachineBlockContent(this));
+    }
+}
